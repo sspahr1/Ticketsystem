f837a6faa0d496002468ae1364f2c5e0187c90d4
Stefan <sspahr@htl-steyr.ac.at>
2020-11-18 10:14:29 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    /**
+     * @ToDo edit run configuration
+     * Open "Run" -> "Edit Configurations..."
+     * Add this to your VM options (just copy & paste the following line):
+     * --module-path "C:\Program Files\Java\javafx-sdk-11.0.2\lib" --add-modules=javafx.controls,javafx.fxml
+     */
+
+    /**
+     * main method
+     *
+     * @param primaryStage
+     * @throws Exception
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
